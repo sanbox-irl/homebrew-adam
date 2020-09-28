0ec243c7df3a897b8dafe0cbf96c22a7a8405f45
Jonathan Spira <jjspira@gmail.com>
2020-09-28 14:09:13 -0700

    homebrew is g2g

--- Formula/adam.rb
+++ Formula/adam.rb
@@ -1,8 +1,8 @@
 class Adam < Formula
     desc "Cross-platform Gms2 cli utility"
     homepage "https://github.com/NPC-Studio/adam"
-    url "https://github.com/NPC-Studio/adam/releases/latest/download/adam-0.1.0-x86_64-apple-darwin.gz"
-    sha256 "2e64dc4ff84410ec5445ce797a54b387eff301ca76bc7ae887e1eed50c74299b"
+    url "https://github.com/NPC-Studio/adam/releases/latest/download/adam-0.1.0-x86_64-apple-darwin.tar.gz"
+    sha256 "a2393429b716ff748b7630519b4404f0a55adb44adcd6f29e300a530431a7326"
     version "0.1.0"
   
     def install
